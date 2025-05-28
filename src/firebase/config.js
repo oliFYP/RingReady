a89ab09f48e5f6871dc7efaabca602f5caf37d1c
Olivier Smith <oliviersmith@Oliviers-MacBook-Pro.local>
2025-05-28 16:04:19 +0100

    added firebase logic

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,18 +1,18 @@
-import { initializeApp } from 'firebase/app';
-import { getAuth } from 'firebase/auth';
-import { getFirestore } from 'firebase/firestore';
-import { getStorage } from 'firebase/storage';
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_STORAGE_BUCKET",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID",
-  measurementId: "YOUR_MEASUREMENT_ID"
+  apiKey: "AIzaSyCZx5Dyb6pphZ3-xBawKZ6DmOwlhQp0BQ8",
+  authDomain: "ringready-afa91.firebaseapp.com",
+  projectId: "ringready-afa91",
+  storageBucket: "ringready-afa91.appspot.com",
+  messagingSenderId: "147220826325",
+  appId: "1:147220826325:web:a24570b586784eaaa9ba5c",
+  measurementId: "G-GHM9VY2QV2",
 };
 
 // Initialize Firebase
